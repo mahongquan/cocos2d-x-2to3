990ee1fe7b6cc0b27c0e2a2ea317448c6570f65d
mahongquan <mahongquan@sina.com>
2015-01-28 20:34:38 +0800

    Update translate.py
    add some class name dictionary

--- translate.py
+++ translate.py
@@ -19,13 +19,13 @@ dic1_2["setIsVisible"]="setVisible"
 for one in tocreate:
     dic1_2[one]="create"
 ########################
-addcc=["Sprite","SpriteFrame","Node","Director","Point","Size","Layer"
+addcc=["GLProgram","TMXLayer","RenderTexture","FiniteTimeAction","TMXTiledMap","TextureCache","Texture2D","Object","Scene","Sprite","SpriteFrame","Node","Director","Point","Size","Layer"
 ,"LayerColor","LayerGradient"
 ,"Rect","Event","Touch","SpriteBatchNode"
 ,"ShaderCache","OrbitCamera","ShaderCache"
 ,"PointArray","Color4B"
 
-,"FadeOut","FadeIn","FadeTo","TintBy","TintTo","Sequence","RepeatForever","CatmullRomTo","CatmullRomBy"
+,"Action","FadeOut","FadeIn","FadeTo","TintBy","TintTo","Sequence","RepeatForever","CatmullRomTo","CatmullRomBy"
 ,"CardinalSplineBy","CardinalSplineTo"
 ,"SkewBy","SkewTo","JumpBy","JumpTo","BezierTo","BezierBy","Place"
 ,"RotateBy","rotateTo","MoveTo","MoveBy","ScaleBy","ScaleTo","ActionInterval"
@@ -39,12 +39,41 @@ addcc=["Sprite","SpriteFrame","Node","Director","Point","Size","Layer"
 dic2_3={}
 dic2_3["objectAtIndex"]="getObjectAtIndex"
 dic2_3["sharedDirector"]="getInstance"
+dic2_3["sharedEngine"]="getInstance"
 dic2_3["CCArray"]="__Array"
 dic2_3["CCString"]="__String"
+dic2_3["ccc3"]="Color3B"
+dic2_3["CCLog"]="log"
+dic2_3["CCObject"]="Ref"
+dic2_3["Object"]="Ref"
+dic2_3["CCSizeMake"]="Size"
+dic2_3["CCSet"]="__Set"
+dic2_3["CCDictionary"]="__Dictionary"
+dic2_3["ccp"]="Point"
+dic2_3["CCRectMake"]="Rect"
+dic2_3["CCPointMake"]="Point"
+dic2_3["sharedAnimationCache"]="getInstance"
+dic2_3["sharedSpriteFrameCache"]="getInstance"
+dic2_3["sharedUserDefault"]="getInstance"
+dic2_3["sharedTextureCache"]="getInstance"
+dic2_3["textureForKey"]="getTextureForKey"
+dic2_3["sharedFileUtils"]="getInstance"
+dic2_3["spriteFrameByName"]="getSpriteFrameByName"
+dic2_3["animationByName"]="getAnimation"
+dic2_3["ccBlendFunc"]="BlendFunc"
+dic2_3["CCPointZero"]="Vec2()"
+dic2_3["tileGIDAt"]="getTileGIDAt"
+dic2_3["ccColor4F"]="Color4F"
+dic2_3["kCCTextAlignmentLeft"]="TextHAlignment::LEFT"
+dic2_3["kCCTextAlignmentRight"]="TextHAlignment::RIGHT"
+dic2_3["kCCTextAlignmentCenter"]="TextHAlignment::CENTER"
+dic2_3["layerNamed"]="getLayer"
+dic2_3["tileAt"]="getTileAt"
+dic2_3["propertiesForGID"]="getPropertiesForGID"
 for one in addcc:
     dic2_3["CC"+one]=one
 ###########################
-dic=dic1_2
+dic=dic2_3
 ###########################
 pattern = re.compile(r"""   (//[^\r\n]*) # match a single line comment
                             | (/\*.*?\*/)      # match a multi line comment
@@ -68,6 +97,7 @@ def func(match):
 def translateStr(source):
   return re.sub(pattern,func,source)
 def translateFile(inputFileName):
+  print inputFileName
   s=codecs.open(inputFileName,"r","utf-8").read()
   return translateStr(s)
 def treatfile(inputFileName):
